a80d9b7344a7da76fd49ab9148c4387313a61871
Nova Mondal <rialmondal@gmail.com>
2024-02-10 01:47:20 -0800

    bug fix in user attendance logging

--- apps/api/src/routes/attendance/handlers/[meeting]/attend.ts
+++ apps/api/src/routes/attendance/handlers/[meeting]/attend.ts
@@ -38,6 +38,7 @@ export default async function attendMeeting(
 		const attendance = await validateUserAttendance(me.attendance);
 
 		if (
+			meeting.attendancePeriod in attendance &&
 			attendance[meeting.attendancePeriod].logs.find(
 				(it) => it.meetingId === meetingId
 			)
